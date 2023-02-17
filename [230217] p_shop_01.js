cdcebc29183c96b8be6ad2a28ef48e97bcf1aa6c
woolen-h <wobada@naver.com>
2023-02-17 18:29:47 +0900

    image and margin

--- [230217] p_shop_01.js
+++ [230217] p_shop_01.js
@@ -1,27 +1,37 @@
 // 로그인 시 팝업 구현
 function showPopup() {
-    var popup = document.getElementById("popup");
-    popup.innerHTML = "로그인 되었습니다.";
-    popup.style.display = "block";
-    setTimeout(hidePopup, 3000); // 3초 후 팝업을 숨깁니다.
-  }
-  
-  function hidePopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "none";
-  }
+  var popup = document.getElementById("popup");
+  popup.innerHTML = "로그인 되었습니다.";
+  popup.style.display = "block";
+  setTimeout(hidePopup, 3000); // 3초 후 팝업을 숨깁니다.
+}
 
-  window.onload = function() {
-    showPopup();
-  };
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
 
-  // search-icon 누를 시 search 텍스트 박스 display
+window.onload = function () {
+  showPopup();
+};
 
-  function showSearch() {
-    var x = document.getElementById("searchbox");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+// search-icon 누를 시 search 텍스트 박스 display
+
+function showSearch() {
+  var x = document.getElementById("searchbox");
+  const searchInput = document.getElementById('search-input');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    searchInput.focus();
+  } else {
+    x.style.display = "none";
   }
+}
+
+// search 버튼 누를 시 자동 커서
+const search = document.getElementById("search");
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", function() {
+  search.focus();
+});
